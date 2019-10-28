19020d3ad7deb9267814c5be93f1ea2669b9994a
Michal Andrzejczak <michal.andrzejcz@gmail.com>
2019-10-28 03:32:16 +0100

    Basic search working, app is getting the look

--- src/js/index.js
+++ src/js/index.js
@@ -1,32 +1,17 @@
 import API_KEY from '../../api.config';
 import axios from 'axios';
+import { isMissing } from './_utils.js';
 import '../sass/main.scss';
 
-
-const queryObject = {
-    searchTitle(query) {
-        statusObject.clear();
-
-        axios.get('http://www.omdbapi.com/', {
-            params: {
-                apikey: API_KEY,
-                type:   'series',
-                s:      query,
-            },
-        })
-        .then(response => {
-            statusObject.loading = true;
-            printResults(response.data.Search);
-        })
-        .catch(error => {
-            statusObject.error        = true;
-            statusObject.errorMessage = error;
-        })
-        .finally(response => {
-            statusObject.completed = true;
-        });
+const resultsObject = {
+    page:          0,
+    searchResults: [],
+    shows:         [],
+    count() {
+        return this.shows.length;
     },
 };
+
 
 const statusObject = {
     loading:      false,
@@ -41,27 +26,128 @@ const statusObject = {
     },
 };
 
-async function printResults(data) {
-    const $searchResults = $('#search-results');
-    const htmlToPrint    = [];
+const queryObject = {
+    async searchTitle(query) {
+        statusObject.clear();
 
-    await data.forEach(show => {
-        let name = show.Title;
-        htmlToPrint.push(`
-            <div class="result-container">
-                <p class="name">${name}</p>
+        await axios.all([
+            axios.get('http://www.omdbapi.com/', {
+                params: {
+                    apikey: API_KEY,
+                    type:   'series',
+                    s:      query,
+                    page:   1,
+                },
+            }),
+            axios.get('http://www.omdbapi.com/', {
+                params: {
+                    apikey: API_KEY,
+                    type:   'series',
+                    s:      query,
+                    page:   2,
+                },
+            }),
+        ])
+        .then(axios.spread((responseFirstPage, responseSecondPage) => {
+            resultsObject.searchResults = [...this.getIds(responseFirstPage.data.Search)];
+        }))
+        .catch(error => {
+            statusObject.error        = true;
+            statusObject.errorMessage = error.message;
+        })
+        .finally(response => {
+            statusObject.completed = true;
+        });
+
+        for (let id of resultsObject.searchResults) {
+            await this.getShowDetails(id).then(response => {
+                resultsObject.shows.push({
+                    id:          response.imdbID,
+                    title:       response.Title,
+                    released:    isMissing(response.Released) ? '<span class="result-element__attr--missing">Missing info</span>' : response.Released,
+                    runtime:     isMissing(response.Runtime) ? `<span class="result-element__attr--missing">Missing info</span>` : response.Runtime,
+                    rating:      isMissing(response.imdbRating) ? '<span class="result-element__attr--missing">Missing info</span>' : response.imdbRating,
+                    description: isMissing(response.Plot) ? '<span class="result-element__attr--missing">Missing Description</span>' : response.Plot.substr(0, 100) + '...',
+                    awarded:     !isMissing(response.Awards),
+                    poster:      isMissing(response.Poster) ? '/assets/default_poster.png' : response.Poster,
+                });
+            });
+        }
+
+        printResults(resultsObject.shows);
+    },
+
+    getShowDetails(id) {
+        return axios.get('http://www.omdbapi.com/', {
+            params: {
+                apikey: API_KEY,
+                i:      id,
+            },
+        })
+        .then(response => {
+                return response.data;
+            },
+        )
+        .catch(error => {
+            statusObject.error        = true;
+            statusObject.errorMessage = error.message;
+        })
+        .finally(response => {
+            statusObject.completed = true;
+        });
+    },
+
+    getIds(searchResults) {
+        return searchResults.map(result => result.imdbID);
+    },
+};
+
+function printResults(dataArray) {
+    const $resultsContainer = $('#results-container');
+    const resultsToPrint    = [];
+
+
+    if (!statusObject.error) {
+        dataArray.forEach(show => {
+            resultsToPrint.push(`
+            <div class="result-element">
+                <span class="result-element__id">IMDB ID ${show.id}</span>
+                ${show.awarded ? `<span class="result-element__awarded">Awarded</span>` : ''}
+                <span class="clearfix"></span>
+                <div class="result-element__grid">
+                    <img class="result-element__poster" src="${show.poster}" alt="${show.title} poster">  
+                    <span class="result-element__title">${show.title}</span>
+                    <span>released <span class="result-element__released">${show.released}</span></span>     
+                    <span>runtime <span class="result-element__runtime">${show.runtime}</span></span>     
+                    <span>IMDB Rating <span class="result-element__rating">${show.rating}</span></span>
+                </div>     
+                <p class="result-element__description">${show.description}</p>     
             </div>
         `);
-    });
+        });
+        $resultsContainer.html(resultsToPrint);
 
-    $searchResults.html(htmlToPrint);
+        return true;
+    }
+
+    const errorMessage = `
+        <div class="error-element">
+            <p class="error-element__heading">Error!</p>
+            <p class="error-element__message">${statusObject.errorMessage}</p>
+        </div>
+    `;
+
+    $resultsContainer.html(errorMessage);
+
+    return false;
 }
 
 (function () {
     $('#search-form').on('submit', function (e) {
         e.preventDefault();
 
-        const query = $('#search-input').val();
+        const query = $('input#search').val();
+
         queryObject.searchTitle(query);
-    });
+    })
 })();
